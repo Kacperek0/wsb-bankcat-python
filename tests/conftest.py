f075b856363e854c54455958d1ddcec702c77a09
Kacperek0 <kacper.szczepanek@gmail.com>
2022-12-21 21:47:09 +0100

    category tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,6 +4,7 @@ import pdb as pdb
 
 sys.path.append('src')
 from db.models.user import User
+from db.models.category import Category
 
 
 @pytest.fixture(autouse=True)
@@ -12,7 +13,15 @@ def create_dummy_user():
     from tests.conf_test_db import override_database_session
     database = next(override_database_session())
 
-    database.query(User).filter(User.email == 'test-user@example.com').delete()
-    database.commit()
+    user = database.query(User).filter(User.email == 'test-user@example.com').first()
+    if user:
+        categories = database.query(Category).filter(Category.user_id == user.id).all()
+        if categories:
+            for category in categories:
+                database.delete(category)
+
+        # Delete user
+        database.delete(user)
+        database.commit()
     # Tu da się zrobi tak, ze na końcu się dropnie.
 
