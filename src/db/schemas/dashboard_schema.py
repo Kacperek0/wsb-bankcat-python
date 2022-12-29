efb000bab4ebcc15033aa7bea4f4cc7c0366936b
Kacperek0 <47487512+Kacperek0@users.noreply.github.com>
2022-12-29 19:51:20 +0100

    dashboards (#8)

---
+++ src/db/schemas/dashboard_schema.py
@@ -0,0 +1,33 @@
+import pydantic
+
+from db.schemas.budget_schema import Budget
+from db.schemas.category_schema import Category
+from db.schemas.financial_record_schema import FinancialRecord
+
+
+class _DashboardBase(pydantic.BaseModel):
+    user_id: int
+
+
+class Dashboard(_DashboardBase):
+    budgets: list[Budget]
+    categories: list[Category]
+
+    class Config:
+        orm_mode = True
+
+
+class DashboardWithFinancialRecords(_DashboardBase):
+    categories: list[Category]
+    financial_records: list[FinancialRecord]
+
+
+    class Config:
+        orm_mode = True
+
+
+class DashboardWithSpendings(_DashboardBase):
+    categories_enriched: list
+
+    class Config:
+        orm_mode = True
